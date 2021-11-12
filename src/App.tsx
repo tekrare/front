84fa009ed66aa1b31b6d73181054e322a05a441d
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-11-12 21:19:27 +0100

    feat(pages/home): add fixed position background rectangles

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="background-rectangle background-rectangle-1" />
+        <div className="background-rectangle background-rectangle-2" />
+        <div className="background-rectangle background-rectangle-3" />
+        <div className="background-rectangle background-rectangle-4" />
+        <div className="background-rectangle background-rectangle-5" />
+        <div className="background-rectangle background-rectangle-6" />
         <Routes>
           <Route path="/" element={<Home isLoggedIn={false} />} />
         </Routes>
